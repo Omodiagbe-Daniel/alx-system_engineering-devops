c128c2289b26e85e7450659028f8334fc8820e10
Omodiagbe-Daniel <nosetale2017@gmail.com>
2022-09-19 05:05:16 +0000

    made correction

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,4 @@
 #client config
-exec { 'echo "PasswordAuthentication no" >> /etc/ssh/ssh_config',
-  path => 'usr/bin'
-}
-exec { 'echo "IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config',
-  path => '/usr/bin'
+exec { 'echo "PasswordAuthentication no\nIdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config':
+        path    => '/bin/'
 }
