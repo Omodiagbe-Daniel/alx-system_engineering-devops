185734926b4f331035ecde7ae561cfe0a3456087
Omodiagbe-Daniel <nosetale2017@gmail.com>
2022-10-18 03:40:30 +0000

    made correction

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,22 +6,22 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    if argv[1]:
-        user_url = "https://jsonplaceholder.typicode.com/users/" + argv[1]
-        employe_name = requests.get(user_url)
-        employee_name = employe_name.json().get("name")
-        url = "https://jsonplaceholder.typicode.com/todos?userId=" + argv[1]
-        task = requests.get(url)
-        tasks = task.json()
-        task_done = 0
-        lists = []
-        for i in range(len(tasks)):
-            if tasks[i]["completed"] is True:
-                lists.append(tasks[i]["title"])
-                task_done += 1
 
-        print("Employee {} is done with tasks({}/{})".format((employee_name),
-              str(task_done), str(20)))
+    user_url = "https://jsonplaceholder.typicode.com/users/" + argv[1]
+    employe_name = requests.get(user_url)
+    employee_name = employe_name.json().get("name")
+    url = "https://jsonplaceholder.typicode.com/todos?userId=" + argv[1]
+    task = requests.get(url)
+    tasks = task.json()
+    task_done = 0
+    lists = []
+    for i in range(len(tasks)):
+        if tasks[i]["completed"] is True:
+            lists.append(tasks[i]["title"])
+            task_done += 1
 
-        for z in range(len(lists)):
-            print("\t{}".format(lists[z]))
+    print("Employee {} is done with tasks({}/{}):".format((employee_name),
+          str(task_done), str(20)))
+
+    for z in range(len(lists)):
+        print("\t{}".format(lists[z]))
